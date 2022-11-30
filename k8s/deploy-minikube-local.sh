0f406afc59cb127421072f6603e5b0c5059ac6c1
Alefe Oliveira <alefe_rod@hotmail.com>
2022-11-30 10:17:07 -0300

    splunk setup

--- k8s/deploy-minikube-local.sh
+++ k8s/deploy-minikube-local.sh
@@ -2,5 +2,7 @@ kubectl rollout restart deployment popular-movies-integration
 kubectl apply -f ./
 aws --endpoint-url=http://192.168.49.2:31240 sqs create-queue --queue-name rate-movie
 
+
+#PERFORMANCE TESTING
 artillery run --output /home/alefe/popular-movies/performance-tests-results/report.json /home/alefe/popular-movies/popular-movies-integration/app/tests/performance-test.yml
 artillery report --output /home/alefe/popular-movies/performance-tests-results/report.html /home/alefe/popular-movies/performance-tests-results/report.json

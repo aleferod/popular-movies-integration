fd6f3165c94e85ef45e7d19e2e4692733dbd6840
Alefe Oliveira <alefe_rod@hotmail.com>
2022-12-06 10:34:27 -0300

    fixing deployment app

--- k8s/deploy-minikube-local.sh
+++ k8s/deploy-minikube-local.sh
@@ -1,8 +1,8 @@
 kubectl -n popular-movies-integration rollout restart deployment popular-movies-integration-app
 kubectl apply -f ./
-aws --endpoint-url=http://192.168.49.2:31554 sqs create-queue --queue-name rate-movie
+aws --endpoint-url=http://192.168.49.2:32622 sqs create-queue --queue-name rate-movie
 
 
 #PERFORMANCE TESTING
-#artillery run --output /home/alefe/popular-movies/performance-tests-results/report.json /home/alefe/popular-movies/popular-movies-integration/app/tests/performance-test.yml
-#artillery report --output /home/alefe/popular-movies/performance-tests-results/report.html /home/alefe/popular-movies/performance-tests-results/report.json
+artillery run --output /home/alefe/popular-movies/performance-tests-results/report.json /home/alefe/popular-movies/popular-movies-integration/app/tests/performance-test.yml
+artillery report --output /home/alefe/popular-movies/performance-tests-results/report.html /home/alefe/popular-movies/performance-tests-results/report.json
